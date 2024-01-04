8afe0dfe3402d011c950ca94de9c4476912cb07a
Udara Dananjaya <budaththanayaka@gmail.com>
2024-01-04 10:20:04 +0530

    Create Login Method

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthController extends Controller
 {
-    public function login(){
-        return 0;
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+        if (!$token = JWTAuth::attempt($credentials)) {
+            return response()->json(['error' => 'Invalid credentials'], 401);
+        }
+        return response()->json(['token' => $token]);
     }
 
     public function checkLogin(){
