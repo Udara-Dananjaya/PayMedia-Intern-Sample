634920fe253e44f0809174315a71b18055440ca9
Udara Dananjaya <budaththanayaka@gmail.com>
2024-01-04 09:54:46 +0530

    Install JWT for Token Authentication
    Update for Testing
    
    Create NOTE.md
    
    Update User Controller  CURD methods
    
    Add ignore for profile images.
    
    Remove unnecessary Validation rules
    
    Update CURD on User Controller
    
    Create CURD on  User Controller
    
    Add User Controller to Api Routes
    
    UserController Create index function
    
    Create User Controller
    
    Update Api Routes with Middleware
    
    Create Methods on AuthController
    
    Create Login Method
    
    Add Auth with JWT on config/auth.php
    
    User Model implement  with JWTSubject
    
    Create JWT Secret
    
    JWT Publish vendor
    
    Create Auth functions
    
    Update Model Users
    
    Update Api Routes
    
    Create Controller AuthController
    
    Update Database on .env
    
    Update Database Migrate User

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        try {
+            return response()->json(User::all(), 200);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
+
+    public function create(Request $request)
+    {
+        try {
+            $request->validate([
+                'name' => 'required|string|max:255',
+                'email' => 'required|email|unique:users',
+                'password' => 'required|string|min:8|max:12|regex:/^.*(?=.{3,})(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[\d\x])(?=.*[!$#%]).*$/',
+                'img' => 'image|mimes:jpeg,png,jpg,gif|max:2048',
+            ]);
+            $user = new User(); // Create a new User instance
+
+            // Set user attributes
+            $user->name = Str::ucfirst($request->input('name'));
+            $user->email = $request->input('email');
+
+            // Hash the password
+            $user->password =  Hash::make($request->input('password'));
+
+            if ($request->hasFile('img')) {
+                $image = $request->file('img');
+                $imageName = 'user_' . $user->id . '.' . $image->getClientOriginalExtension();
+                $image->move(public_path('images'), $imageName);
+                $user->img = 'images/' . $imageName;
+            }
+
+            $user->save();
+
+            return response()->json($user, 201);
+        } catch (ValidationException $e) {
+            // Handle validation errors
+            return response()->json(['error' => $e->errors()], 422);
+        } catch (\Exception $e) {
+            // Handle other exceptions
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
+
+    public function update(Request $request, $id)
+    {
+
+        try {
+            $request->validate([
+                'name' => 'string|max:255',
+                'email' => 'email|unique:users',
+                'password' => 'nullable|string|min:8|max:12|regex:/^.*(?=.{3,})(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[\d\x])(?=.*[!$#%]).*$/',
+                'img' => 'image|mimes:jpeg,png,jpg,gif|max:2048',
+            ]);
+
+            $user = User::findOrFail($id);// Find the user by ID
+
+            // Update user attributes
+            $user->name = $request->filled('name') ? Str::ucfirst($request->input('name')) : $user->name;
+            $user->email = $request->filled('email') ? $request->input('email') : $user->email;
+
+            if ($request->has('password')) {
+                // Update the password only if provided
+                $user->password =  Hash::make($request->input('password'));
+            }
+
+            // Update the image if provided
+            if ($request->hasFile('img')) {
+                $image = $request->file('img');
+                $imageName = 'user_' . $user->id . '.' . $image->getClientOriginalExtension();
+                $image->move(public_path('images'), $imageName);
+                $user->img = 'images/' . $imageName;
+            }
+
+            $user->save();// Save the user
+
+            return response()->json($user, 200);
+        } catch (ValidationException $e) {
+            // Handle validation errors
+            return response()->json(['error' => $e->errors()], 422);
+        } catch (\Exception $e) {
+            // Handle other exceptions
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
+
+    public function show($id)
+    {
+        try {
+            return response()->json(User::find($id), 200);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
+
+    public function delete($id)
+    {
+        try {
+            $user = User::findOrFail($id);
+            $user->delete();
+            return response()->json(['status' => true], 200);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +19,34 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group([],function (){
+    Route::post('login',[AuthController::class, 'login']);
+ });
+
+// Route::middleware(['auth:api'])->group(function () {
+//     Route::post('checkToken', [AuthController::class, 'checkToken']);
+//     Route::post('logout', [AuthController::class, 'logout']);
+//     Route::post('list', [UserController::class, 'index']);
+
+//     Route::post('create', [UserController::class, 'create']);
+//     Route::post('update/{id}', [UserController::class, 'update']);
+
+//     Route::post('list/{id}', [UserController::class, 'show']);
+//     Route::post('delete/{id}', [UserController::class, 'delete']);
+
+// });
+
+Route::group([],function () {
+    Route::post('checkToken', [AuthController::class, 'checkToken']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::get('list', [UserController::class, 'index']);
+
+    Route::post('create', [UserController::class, 'create']);
+    Route::post('update/{id}', [UserController::class, 'update']);
+
+    Route::post('list/{id}', [UserController::class, 'show']);
+    Route::post('delete/{id}', [UserController::class, 'delete']);
+
 });
