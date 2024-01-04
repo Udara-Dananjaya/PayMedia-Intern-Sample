d075804a87d1e82c3e7a49a6d451e44ee5da7bf2
Udara Dananjaya <budaththanayaka@gmail.com>
2024-01-04 10:00:56 +0530

    Update Api Routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\AuthController;
+//use App\Http\Controllers\Api\AdminController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +20,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group([],function (){
+    Route::post('login',[AuthController::class, 'login']);
+    Route::post('checkToken',[AuthController::class, 'checkToken']);
+    Route::post('logout',[AuthController::class, 'checklogoutToken']);
+   //  Route::post('admin',[AdminController::class, 'index']);
+ 
+ });
