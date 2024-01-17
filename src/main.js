4bfce5e4444d778359d58693a24de0a89df702e2
Udara Dananjaya <dev.dananjaya@gmail.com>
2024-01-17 13:21:55 +0530

    Finalize

--- src/main.js
+++ src/main.js
@@ -3,16 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'; // Make sure the path is correct
 
-import BootstrapVue from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 import VueSweetalert2 from 'vue-sweetalert2';
 import 'sweetalert2/dist/sweetalert2.min.css'
 
 Vue.config.productionTip = false
 
 Vue.use(VueSweetalert2);
-Vue.use(BootstrapVue)
 
 new Vue({
   router,
